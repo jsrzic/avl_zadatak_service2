01c607a96c0b57179657c6716171ac23dc05c390
jsrzic <josip.srzic@gmail.com>
2023-07-06 11:21:17 +0200

    add config.json file, updated Dockerfile and service2.py correspondingly

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,7 @@ FROM python:3
 RUN curl -sL https://github.com/openfaas/faas/releases/download/0.9.14/fwatchdog > /usr/bin/fwatchdog \
     && chmod +x /usr/bin/fwatchdog
 
-ENV fprocess="python3 service2.py"
-ENV SERVICE1_URL="http://service1:8080"
-ENV HASH_FUNCTION="md5"
+ENV fprocess="python3 service2.py config.json"
 
 WORKDIR /app
 
--- service2.py
+++ service2.py
@@ -1,12 +1,21 @@
 import requests
 import sys
-import os
+import json
 
-SERVICE1_URL = os.getenv('SERVICE1_URL')
-HASH_FUNCTION = os.getenv('HASH_FUNCTION')
+def main():
+    with open(sys.argv[1], 'r') as config_file:
+        config = json.load(config_file)
 
-message = requests.get(sys.stdin.readline()).text
+    SERVICE1_ALIAS = config['SERVICE1_ALIAS']
+    SERVICE1_PORT = config['SERVICE1_PORT']
+    HASH_FUNCTION = config['HASH_FUNCTION']
+    SERVICE1_URL = f'http://{SERVICE1_ALIAS}:{SERVICE1_PORT}'
 
-data = [HASH_FUNCTION, message]
+    message = requests.get(sys.stdin.readline()).text
 
-print(requests.post(SERVICE1_URL, data="\n".join(data)).text)
+    data = [HASH_FUNCTION, message]
+
+    print(requests.post(SERVICE1_URL, data="\n".join(data)).text)
+
+if __name__ == "__main__":
+    main()
