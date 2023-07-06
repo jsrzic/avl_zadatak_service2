62185486038ca580f2c4aedd52f2213e26bbe678
jsrzic <josip.srzic@gmail.com>
2023-07-06 23:00:47 +0200

    removed config file setup, added input through env variables

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,14 @@ FROM python:3
 RUN curl -sL https://github.com/openfaas/faas/releases/download/0.9.14/fwatchdog > /usr/bin/fwatchdog \
     && chmod +x /usr/bin/fwatchdog
 
-ENV fprocess="python3 service2.py config.json"
+ENV fprocess="python3 service2.py"
 
 WORKDIR /app
 
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY service2.py config.json /app/
+COPY service2.py /app/
 
 EXPOSE 8080
 CMD [ "fwatchdog" ]
--- service2.py
+++ service2.py
@@ -1,14 +1,11 @@
 import requests
 import sys
-import json
+import os
 
 def main():
-    with open(sys.argv[1], 'r') as config_file:
-        config = json.load(config_file)
-
-    SERVICE1_ALIAS = config['SERVICE1_ALIAS']
-    SERVICE1_PORT = config['SERVICE1_PORT']
-    HASH_FUNCTION = config['HASH_FUNCTION']
+    SERVICE1_ALIAS = os.getenv('SERVICE1_ALIAS')
+    SERVICE1_PORT = os.getenv('SERVICE1_PORT')
+    HASH_FUNCTION = os.getenv('HASH_FUNCTION')
     SERVICE1_URL = f'http://{SERVICE1_ALIAS}:{SERVICE1_PORT}'
 
     message = requests.get(sys.stdin.readline()).text
